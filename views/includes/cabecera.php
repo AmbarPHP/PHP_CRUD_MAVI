b82b1b5f882898ea94157b3d7b39ee5599b43697
AmbarPHP <perlamarina.franco@gmail.com>
2023-07-06 17:08:49 -0600

    Initial commit

---
+++ views/includes/cabecera.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>CRUD con PHP - AJAX</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- styles -->
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/starter-template.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+
+<!--     <script src="./assets/js/jquery.min.js"></script> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="./assets/js/bootstrap.min.js"></script>
+</head>
+<body>
