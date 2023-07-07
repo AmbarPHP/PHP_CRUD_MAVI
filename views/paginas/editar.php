c49423df45b1d73d1af0a9cae5a22e8717dd2647
AmbarPHP <perlamarina.franco@gmail.com>
2023-07-06 19:23:36 -0600

    Edicion

--- views/paginas/editar.php
+++ views/paginas/editar.php
@@ -16,6 +16,9 @@
     	$datos = array(
 			'nombre'   	=> $_POST['nombre'],
 			'email'    	=> $_POST['email'],
+			'domicilio'   => $_POST['domicilio'],
+			'apellido_paterno'    => $_POST['apellido_paterno'],
+			'apellido_materno'    => $_POST['apellido_materno'],
 		);
 		$objeto->editarCliente($id, $datos);
     }
@@ -24,7 +27,7 @@
 	<main role="main" class="container">
 
 		<div class="starter-template">
-			<h1>CRUD sencillo con PHP + AJAX</h1>
+			<h1>Editar registro</h1>
 			<hr>
 			<div class="col-md-6 offset-3">
 				<form action="index.php?page=editar" method="POST" name="editarForm" id="editarForm" class="text-left">
@@ -39,10 +42,26 @@
 							<small id="nombreHelp" class="form-text text-muted">Ingrese el nombre completo del cliente.</small>
 						</div>
 						<div class="form-group">
-							<label for="email">E-mail</label>
-							<input type="email" id="email" name="email" class="form-control" aria-describedby="emailHelp" value="<?= $r['email']; ?>">
-							<small id="emailHelp" class="form-text text-muted">Ingrese el correo electronico del cliente.</small>
+						<label for="email">Apellido paterno</label>
+						<input type="text" id="paterno" name="paterno" class="form-control" aria-describedby="paternoHelp">
+						<small id="domicilioHelp" class="form-text text-muted">Ingrese apellido paterno </small>
 						</div>
+						<div class="form-group">
+						<label for="email">Apellido materno</label>
+						<input type="text" id="materno" name="materno" class="form-control" aria-describedby="maternoHelp">
+						<small id="domicilioHelp" class="form-text text-muted">Ingrese apellido materno </small>
+						</div>
+
+						<div class="form-group">
+						<label for="email">E-mail</label>
+						<input type="email" id="email" name="email" class="form-control" aria-describedby="emailHelp" value="<?= $r['email']; ?>">
+						<small id="emailHelp" class="form-text text-muted">Ingrese el correo electronico del cliente.</small>
+						</div>
+					<div class="form-group">
+						<label for="email">Domicilio</label>
+						<input type="text" id="domicilio" name="domicilio" class="form-control" aria-describedby="domicilioHelp">
+						<small id="domicilioHelp" class="form-text text-muted">Ingrese el domicilio </small>
+					</div>
 						<button type="submit" name="editar" class="btn btn-info">Editar registro</button>
 					<?php } } ?>
 				</form>
--- views/paginas/insertar.php
+++ views/paginas/insertar.php
@@ -7,6 +7,9 @@ if (isset($_POST['btnInsertar'])) {
 	$datos = array(
 		'nombre'   => $_POST['nombre'],
 		'email'    => $_POST['email'],
+		'domicilio'   => $_POST['domicilio'],
+		'apellido_paterno'    => $_POST['apellido_paterno'],
+		'apellido_materno'    => $_POST['apellido_materno'],
 	);
 	$objeto->insertarCliente($datos);
 }
